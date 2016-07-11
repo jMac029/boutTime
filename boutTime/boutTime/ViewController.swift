9380709b07386697764f5866e4561c0ac351eb91
James McMillan <jmcmillan@hri.uci.edu>
2016-07-11 15:33:29 -0700

    changed EventLabels to eventButtons so that WebView feature will work later on. Connected all labels and buttons to the ViewController

--- boutTime/boutTime/ViewController.swift
+++ boutTime/boutTime/ViewController.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+// MARK: Labels
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var instructionLabel: UILabel!
+    
+// MARK: Buttons
+    
+    @IBOutlet weak var eventButton01: UIButton!
+    @IBOutlet weak var eventButton02: UIButton!
+    @IBOutlet weak var eventButton03: UIButton!
+    @IBOutlet weak var eventButton04: UIButton!
+    
+    @IBOutlet weak var downButton01: UIButton!
+    @IBOutlet weak var upButton01: UIButton!
+    @IBOutlet weak var downButton02: UIButton!
+    @IBOutlet weak var upButton02: UIButton!
+    @IBOutlet weak var downButton03: UIButton!
+    @IBOutlet weak var upButton03: UIButton!
+    
+    @IBOutlet weak var endRoundButton: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
